f952ad7f844066ba36d54a0f00a4a0cbe97cb1aa
thapadevendra <devendraido@gmail.com>
2022-08-01 13:18:39 -0500

    add missing routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,5 +4,10 @@ export default[
         path: "/admindashboard",
         name: "surveyList",
         component: () => import("@/views/Admin/AdminDashboard.vue"),
+    },
+    {
+        path: "/admindashboard/viewSurvey",
+        name: "viewSurvey",
+        component: () => import("@/views/Admin/ViewSurvey.vue"),
     }
 ]
