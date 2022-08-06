1c147dfd07830c32e63f1ee96e4f474f18e25c60
thapadevendra <devendraido@gmail.com>
2022-08-06 17:12:30 -0500

    fix

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -25,12 +25,14 @@ export default[
         path: "/:surveyID/surveyForm/respondent/:respondentID",
         name: "respondentSurveyForm",
         component: () => import("@/views/Admin/RespondentSurveyForm.vue"),
-    }
+    },
+    
+    {
 
         path: "/admindashboard/viewResponses/:surveyID",
         name: "viewResponses",
         component: () => import("@/views/Admin/ViewResponses.vue"),
-      },
+    }
      
 
 ]
